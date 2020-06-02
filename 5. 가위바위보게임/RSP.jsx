58825bed602b1b473744274c4d9851ad82ffadef
yjcho <jinjanic91@gmail.com>
2020-06-03 00:21:04 +0900

    가위바위보 게임로직작성완료

--- 5. 가위바위보게임/RSP.jsx
+++ 5. 가위바위보게임/RSP.jsx
@@ -21,6 +21,12 @@ const scores = {
    보: -1,
 };
 
+const computerChoice = (imgCoord) => {
+   return Object.entries(rspCoords).find(function(v) {
+      return v[1] === imgCoord;
+   })[0];
+};
+
 
 class RSP extends Component {
    state = {
@@ -40,27 +46,12 @@ class RSP extends Component {
       //인터벌 예시
       //이 셋인터벌은 RSP 컴포넌트가 없어져도 그대로 남아서 돌아가게 되므로, 
       //아래에서 제거를 해주어야 한다.
-       this.interval = setInterval(() => { //1초마다 가위바위보 돌아감
-         const {imgCoord} = this.state; //비동기 함수인 경우, 비동기 함수 바깥에 있는 변수 참조하면 클로저 문제가 발생!! 주의해야 함! 
-         if(imgCoord === rspCoords.바위) {
-            this.setState({
-                imgCoord: rspCoords.가위,
-             });
-          } else if(imgCoord === rspCoords.가위) {
-            this.setState({
-               imgCoord: rspCoords.보,
-            });
-          } else if(imgCoord === rspCoords.보) {
-            this.setState({
-               imgCoord: rspCoords.바위,
-            });
-          }
-       }, 1000);
+       this.interval = setInterval(this.changeHand, 100);
    }
 
    //setState나 props가 바뀌었거나 등 리렌더링이 되는 경우, 리렌더링 후 실행되는 부분
    componentDidUpdate() {
-
+      //요번 예시에서는 쓰이지 않음.
    }
 
    //컴포넌트가 제거되기 직전에 실행 됨
@@ -68,11 +59,55 @@ class RSP extends Component {
    componentWillUnmount() { // -> 여기에 비동기 요청 정리를 많이 한다.
       //인터벌 예시
       //위에서 셋 인터벌 해준걸 지우지 않으면, 메모리 누수가 생김
-      //clearInterval(this.interval);
+      clearInterval(this.interval);
    }
 
-   onClickBtn = (choice) => {
+   changeHand = () => { //1초마다 가위바위보 돌아감
+      const {imgCoord} = this.state; //비동기 함수인 경우, 비동기 함수 바깥에 있는 변수 참조하면 클로저 문제가 발생!! 주의해야 함! 
+      if(imgCoord === rspCoords.바위) {
+         this.setState({
+             imgCoord: rspCoords.가위,
+          });
+       } else if(imgCoord === rspCoords.가위) {
+         this.setState({
+            imgCoord: rspCoords.보,
+         });
+       } else if(imgCoord === rspCoords.보) {
+         this.setState({
+            imgCoord: rspCoords.바위,
+         });
+       }
+    }
 
+   onClickBtn = (choice) => {
+      const {imgCoord} = this.state;
+      clearInterval(this.interval);
+      const myScore = scores[choice];
+      const cpuScore = scores[computerChoice(imgCoord)];
+      const diff = myScore - cpuScore;
+
+      if(diff === 0) {
+         this.setState({
+            result: '비겼습니다!',
+         });
+      } else if ([-1, 2].includes(diff)) {
+         this.setState((prevState) => {
+            return {
+               result: '이겼습니다',
+               score: prevState.score + 1,
+            };
+         });
+      } else {
+         this.setState((prevState) => {
+            return {
+               result: '졌습니다ㅠ',
+               score: prevState.score - 1,
+            };
+         });
+      }
+      setTimeout(() => {
+         this.interval = setInterval(this.changeHand, 100);
+      }, 2000);
    };
 
    render() {
