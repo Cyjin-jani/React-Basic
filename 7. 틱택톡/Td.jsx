3da1716e5eb2ae20e19e16d6c7a41154942db743
yjcho <jinjanic91@gmail.com>
2020-06-19 23:54:05 +0900

    테이블 최적화 완료

--- 7. 틱택톡/Td.jsx
+++ 7. 틱택톡/Td.jsx
@@ -1,8 +1,15 @@
-import React , { useCallback } from 'react';
+import React , { useCallback, useEffect, useRef, memo } from 'react';
 import { CLICK_CELL } from './TicTacToe';
 
-const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
+const Td = memo(({ rowIndex, cellIndex, dispatch, cellData }) => {
     console.log('td rendered');
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3]); //true true true false가 나옴... cellData가 바뀐것
+        //바뀌지 않는 경우가 가끔 있어서 그것을 확인하는 작업임
+        ref.current = [rowIndex, cellIndex, dispatch, cellData];
+    }, [rowIndex, cellIndex, dispatch, cellData]);
 
     const onClickTd = useCallback( () => {
         console.log(rowIndex, cellIndex);
@@ -19,6 +26,6 @@ const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
     return (
     <td onClick={onClickTd}>{cellData}</td>
     );
-}
+});
 
 export default Td;
--- 7. 틱택톡/Tr.jsx
+++ 7. 틱택톡/Tr.jsx
@@ -1,14 +1,25 @@
-
-import React from 'react';
+import React, { useRef, useEffect, memo, useMemo } from 'react';
 import Td from './Td';
 
- const Tr = ({ rowData, rowIndex, dispatch }) => {
+ const Tr = memo(({ rowData, rowIndex, dispatch }) => {
+    console.log('tr rendered');
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowData === ref.current[0], rowIndex === ref.current[1], dispatch === ref.current[2]); //true true true false가 나옴... cellData가 바뀐것
+        //바뀌지 않는 경우가 가끔 있어서 그것을 확인하는 작업임
+        ref.current = [rowData, rowIndex, dispatch];
+    }, [rowData, rowIndex, dispatch]);
 
     return (
     <tr>
-    {Array(rowData.length).fill().map((td, i) => (<Td dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>))}
+    {Array(rowData.length).fill().map((td, i) => (
+    useMemo( //컴포넌트 자체를 이렇게 유즈메모로 기억할 수 있다
+    () => <Td key={i} dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>, [rowData[i]],
+    )
+    ))}
     </tr>
     );
-};
+});
 
  export default Tr;
